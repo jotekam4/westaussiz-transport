1278a28cdaa9206d23fd8736cebefb2c7cb72a74
ekamjot-singh <jotekam4@gmail.com>
2024-07-17 14:53:24 +1000

    Under Construction Page, Initialising the website page.

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="items-center justify-between p-24">
+      <h1>
+        WestAussiz Transport PTY LTD.
+      </h1>
+
+      <p>
+        Sorry, This website is under production. Hope to see you soon.
+      </p>
+
+    </main>
+  );
+}
